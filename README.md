aa7d64fd21209a498196630fe0e939831aeb837b
noonesimg <noonesimage@gmail.com>
2024-02-19 11:53:04 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# vue-ts
+
+Minimal Vue3 + Vite template with typescript support 
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const count = ref(0);
+</script>
+
+<template>
+  <div class="main-container">
+    <h3 @click="count++" @contextmenu.prevent="count=0">
+      Test Counter {{ count }}
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+.main-container {
+  user-select: none;
+}
+</style>
